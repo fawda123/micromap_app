3fba05ad1ac182d754973dbceb4b9da14ef96847
fawda123 <mbafs2012@gmail.com>
2016-10-12 10:25:50 -0500

    app deploployed

---
+++ server.R
@@ -0,0 +1,52 @@
+library(shiny)
+library(micromap)
+library(datasets)
+
+# data
+data(states)
+statdat <- state.x77
+
+# spatial data
+data(USstates)
+
+shinyServer(function(input, output) {
+   
+  output$p1 <- renderPlot({
+
+    # include median value, gruopings
+    medval <- as.logical(input$medval)
+    grps <- input$grps
+
+    # input variables, dates, and order
+    vr1 <- input$vr1
+    vr2 <- input$vr2
+
+    ords <- input$ords
+    ords <- gsub('Variable\\s', 'vr', ords)
+    
+    # subset variables and dates, combine
+    toplo <- statdat[, c(vr1, vr2)]
+    toplo <- data.frame(toplo)  
+    names(toplo) <- c('vr1', 'vr2')
+    toplo$ST <- row.names(toplo)
+    
+    # create map table for polygons
+    polys <- create_map_table(USstates, 'ST_NAME')
+
+    # plot
+    mmplot(stat.data = toplo,
+      map.data = polys,
+      panel.types = c('labels', 'dot', 'dot', 'map'),
+      panel.data = list('ST', 'vr1', 'vr2', NA),
+      ord.by = ords,  
+      median.row = medval,
+      grouping = grps,
+      map.link = c('ST', 'ID'), 
+        panel.att = list(
+          list(2, header = vr1),
+          list(3, header = vr2)
+        ))
+
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,86 @@
+library(shiny)
+library(shinythemes)
+library(micromap)
+library(datasets)
+
+# data
+data(states)
+statdat <- state.x77
+
+# spatial data
+data(USstates)
+
+# variables
+vrs <- colnames(statdat)
+
+# Define UI for application
+shinyUI(fluidPage(
+  
+  theme = shinytheme('journal'), 
+  
+  # main panel
+  mainPanel(width = 12,
+      
+    # spacing
+    fluidRow(HTML('<p><h3>Micromap Shiny Application</h3></p>')),
+    
+    #spacing
+    fluidRow(HTML('<p></p>')),
+      
+    # first row of plots
+    fluidRow(
+      
+      column(width = 2, 
+        selectInput(inputId = 'vr1', 
+          label = h4('Variable 1'),
+          choices = vrs,
+          selected = vrs[1]
+          )
+        ),
+      
+      column(width = 2, 
+        selectInput(inputId = 'vr2', 
+          label = h4('Variable 2'),
+          choices = vrs,
+          selected = vrs[2]
+          )
+        ),
+      
+      column(width = 2, 
+        numericInput("grps", 
+          label = h4("Groupings"), 
+          value = 10, 
+          min = 1, 
+          max = 50
+          )
+        ),
+      
+      column(width = 2,
+        radioButtons(inputId = 'medval', 
+          label = h4('Median value?'),
+          choices = c(TRUE, FALSE),
+          selected = c(FALSE), 
+          inline = TRUE
+          )
+        ),
+      
+      column(width = 3,
+        radioButtons(inputId = 'ords', 
+          label = h4('Order by'),
+          choices = c('Variable 1', 'Variable 2'),
+          selected = c('Variable 1'), 
+          inline = TRUE
+          )
+        ),
+      
+      column(width = 12, 
+        
+        plotOutput("p1", width = '1000px', height = '750px')
+        
+        )
+      
+      ) 
+    
+    )
+        
+))
