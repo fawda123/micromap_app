c785e3ef109fa1d6259a77dec02dd34132bd0d30
fawda123 <mbafs2012@gmail.com>
2022-05-08 21:25:24 -0400

    formatting for server and ui to match chapter, redeployed

--- server.R
+++ server.R
@@ -10,44 +10,44 @@ data(USstates)
 
 # define server for the application
 server <- function(input, output) {
-   
+  
   output$p1 <- renderPlot({
-
+    
     # include median value, groupings
-    medval <- as.logical(input$medval)
+    medval <- as.logical(x = input$medval)
     grps <- input$grps
-
+    
     # input variables, dates, and order
     vr1 <- input$vr1
     vr2 <- input$vr2
-
+    
     # name the variable for ordering
     ords <- input$ords
-    ords <- gsub('Variable\\s', 'vr', ords)
+    ords <- gsub(pattern = "Variable\\s", replacement = "vr", x = ords)
     
     # subset variables and dates, combine
     toplo <- statdat[, c(vr1, vr2)]
     toplo <- data.frame(toplo)  
-    names(toplo) <- c('vr1', 'vr2')
-    toplo$ST <- row.names(toplo)
+    names(toplo) <- c("vr1", "vr2")
+    toplo$ST <- row.names(x = toplo)
     
     # create map table for polygons
-    polys <- create_map_table(USstates, 'ST_NAME')
-
+    polys <- create_map_table(tmp.map = USstates, IDcolumn = "ST_NAME")
+    
     # plot
     mmplot(stat.data = toplo,
-      map.data = polys,
-      panel.types = c('labels', 'dot', 'dot', 'map'),
-      panel.data = list('ST', 'vr1', 'vr2', NA),
-      ord.by = ords,  
-      median.row = medval,
-      grouping = grps,
-      map.link = c('ST', 'ID'), 
-        panel.att = list(
-          list(2, header = vr1),
-          list(3, header = vr2)
-        ))
-
+           map.data = polys,
+           panel.types = c("labels", "dot", "dot", "map"),
+           panel.data = list("ST", "vr1", "vr2", NA),
+           ord.by = ords,  
+           median.row = medval,
+           grouping = grps,
+           map.link = c("ST", "ID"), 
+           panel.att = list(
+             list(2, header = vr1),
+             list(3, header = vr2)
+           ))
+    
   })
   
 }
--- ui.R
+++ ui.R
@@ -6,62 +6,62 @@ library(datasets)
 statdat <- state.x77
 
 # variables
-vrs <- colnames(statdat)
+vrs <- colnames(x = statdat)
 
 # define ui for application
 ui <- fluidPage(
   
-  theme = shinytheme('journal'), 
+  theme = shinytheme(theme = "journal"), 
   
-  titlePanel('Micromap Shiny Application'), 
+  titlePanel(title = "Micromap Shiny Application"), 
   
   sidebarLayout(
     
     # side bar
     sidebarPanel(width = 2,
-      
-      selectInput(inputId = 'vr1', 
-                  label = h4('Variable 1'),
-                  choices = vrs,
-                  selected = vrs[1]
-      ),
-      
-      selectInput(inputId = 'vr2', 
-                  label = h4('Variable 2'),
-                  choices = vrs,
-                  selected = vrs[2]
-      ),
-      
-      numericInput("grps", 
-                   label = h4("Groupings"), 
-                   value = 10, 
-                   min = 1, 
-                   max = 50
-      ),
-      
-      radioButtons(inputId = 'medval', 
-                   label = h4('Median value?'),
-                   choices = c(TRUE, FALSE),
-                   selected = c(FALSE), 
-                   inline = TRUE
-      ),
-      
-      radioButtons(inputId = 'ords', 
-                   label = h4('Order by'),
-                   choices = c('Variable 1', 'Variable 2'),
-                   selected = c('Variable 1'), 
-                   inline = TRUE
-      )
-      
+                 
+                 selectInput(inputId = "vr1", 
+                             label = h4("Variable 1"),
+                             choices = vrs,
+                             selected = vrs[1]
+                 ),
+                 
+                 selectInput(inputId = "vr2", 
+                             label = h4("Variable 2"),
+                             choices = vrs,
+                             selected = vrs[2]
+                 ),
+                 
+                 numericInput(inputId = "grps", 
+                              label = h4("Groupings"), 
+                              value = 10, 
+                              min = 1, 
+                              max = 50
+                 ),
+                 
+                 radioButtons(inputId = "medval", 
+                              label = h4("Median value?"),
+                              choices = c(TRUE, FALSE),
+                              selected = c(FALSE), 
+                              inline = TRUE
+                 ),
+                 
+                 radioButtons(inputId = "ords", 
+                              label = h4("Order by"),
+                              choices = c("Variable 1", "Variable 2"),
+                              selected = c("Variable 1"), 
+                              inline = TRUE
+                 )
+                 
     ), 
     
-  # main panel
-  mainPanel(
-  
-      plotOutput("p1", width = '700px', height = '750px')
+    # main panel
+    mainPanel(
+      
+      plotOutput(outputId = "p1", width = "700px", height = "750px")
       
     )
-  
+    
   )
-        
+  
 )
